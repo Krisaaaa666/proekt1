6640e383afe7107999d01e4eced5944a9dc23600
Кристина <@DESKTOP-E6N9PBR>
2023-12-23 22:49:00 +0300

    Добавьте файлы проекта.

---
+++ proekt1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentenceProcessing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите предложение: ");
+            string input = Console.ReadLine();
+            string[] words = SplitText(input);
+            PrintWords(words);
+        }
+
+        /// <summary>
+        /// Метод разбивает введенное предложение на отдельные слова и сохраняет их в массив words.
+        ///</summary>
+        static string[] SplitText(string text)
+        {
+            string[] words = text.Split(' ');
+            return words;
+        }
+
+        /// <summary>
+        /// Метод выводит на экран каждое слово из массива words.
+        /// </summary>
+        static void PrintWords(string[] words)
+        {
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+            Console.ReadKey();
+        }
+    }
+}               
